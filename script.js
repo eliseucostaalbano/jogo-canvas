429e1bc76554b890634650f068f52f886869223a
Eliseu costa <eliseualbano57@gmail.com>
2024-08-30 13:56:48 -0300

    Criando o jogador

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const ctx = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+// Criando o jogador
+class Jogador {
+    constructor(x, y, radius, cor) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = cor
+    }
+
+    desenhar() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+}
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const jogador = new Jogador(x, y, 30, 'darkblue')
+jogador.desenhar()
