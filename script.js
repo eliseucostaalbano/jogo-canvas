0660c4a2c550a8127633de9c283e9579de30f136
Eliseu costa <eliseualbano57@gmail.com>
2024-09-05 13:31:42 -0300

    criando e desenhando projeteis

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ canvas.height = innerHeight
 
 // Criando o jogador
 class Jogador {
-    constructor(x, y, radius, cor) {
+    constructor(x, y, tamanho, cor) {
         this.x = x
         this.y = y
-        this.radius = radius
+        this.radius = tamanho
         this.color = cor
     }
 
@@ -22,8 +22,34 @@ class Jogador {
     }
 
 }
+
+// Criando projetil
+class Projetil {
+    constructor(x, y, tamanho, cor, velocidade) {
+        this.x = x
+        this.y = y
+        this.radius = tamanho
+        this.color = cor
+        this.velo = velocidade
+    }
+
+    desenhar() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
+// desenhando o jogador
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const jogador = new Jogador(x, y, 30, 'darkblue')
 jogador.desenhar()
+
+// desenhando os projeteis
+addEventListener('click', (event) => {
+    const tiro = new Projetil(event.clientX , event.clientY , 5 , 'black', null)
+    tiro.desenhar()
+})
