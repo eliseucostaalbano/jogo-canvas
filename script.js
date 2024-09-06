a9a129db930c5e2bc667255448265a3ca1372ea2
Eliseu costa <eliseualbano57@gmail.com>
2024-09-06 14:00:47 -0300

    Criando inimigos

--- script.js
+++ script.js
@@ -48,22 +48,73 @@ class Projetil {
     }
 }
 
-// desenhando o jogador
+// Criando inimigos
+class Inimigo {
+    constructor(x, y, tamanho, cor, velocidade) {
+        this.x = x
+        this.y = y
+        this.radius = tamanho
+        this.color = cor
+        this.velo = velocidade
+    }
+
+    desenhar() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.desenhar()
+        this.x = this.x + this.velo.x
+        this.y = this.y + this.velo.y
+
+    }
+}
+
+// Criando as variaveis do jogo
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const jogador = new Jogador(x, y, 30, 'darkblue')
-
-
-// desenhando os projeteis
 const tiros = []
+const inimigos = []
 
+function spawnInimigos(){
+   setInterval(() =>{
+   const tamanho  = Math.random() * (30 - 5) + 5
+   let x 
+   let y 
+   if(Math.random() < 0.5){
+     x = Math.random() < 0.5 ? 0 - tamanho : canvas.width + tamanho
+     y = Math.random() * canvas.height
+    //  y = Math.random() < 0.5 ? 0 - tamanho : canvas.height + tamanho
+   } else{
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - tamanho : canvas.height + tamanho
+   }
+   const cor  = 'black'
+   const angulo = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+   const velo = {
+     x: Math.cos(angulo),
+     y: Math.sin(angulo)
+   }
+
+    inimigos.push(new Inimigo(x, y, tamanho, cor, velo))
+    } , 1000)
+}
+// desenhando o jogo
 function animação() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     jogador.desenhar()
     requestAnimationFrame(animação)
     tiros.forEach((tiro) => {
         tiro.update()
+    })
+
+    inimigos.forEach(inimigo => {
+        inimigo.update()
     })
 }
 
@@ -75,7 +126,8 @@ addEventListener('click', (event) => {
      y: Math.sin(angulo)
    }
 
-    tiros.push(new Projetil(canvas.width / 2, canvas.height / 2, 5, 'red', velo))
+    tiros.push(new Projetil(canvas.width / 2, canvas.height / 2, 5, 'silver', velo))
 })
 
 animação()
+spawnInimigos()
