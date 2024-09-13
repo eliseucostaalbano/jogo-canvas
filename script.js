1197677b4a9f20938ca394b5727fd70dc97bf723
Eliseu costa <eliseualbano57@gmail.com>
2024-09-13 15:06:38 -0300

    criando condição para remover projeteis nas pontas da tela

--- script.js
+++ script.js
@@ -80,68 +80,78 @@ const y = canvas.height / 2
 const jogador = new Jogador(x, y, 30, 'darkblue')
 const tiros = []
 const inimigos = []
-let animaçaoId 
+let animaçaoId
 // Criando  as  funções do jogo
-function spawnInimigos(){
-   setInterval(() =>{
-   const tamanho  = Math.random() * (30 - 5) + 5
-   let x 
-   let y 
-   if(Math.random() < 0.5){
-     x = Math.random() < 0.5 ? 0 - tamanho : canvas.width + tamanho
-     y = Math.random() * canvas.height
-   } else{
-      x = Math.random() * canvas.width
-      y = Math.random() < 0.5 ? 0 - tamanho : canvas.height + tamanho
-   }
-   const cor  = 'black'
-   const angulo = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
-   const velo = {
-     x: Math.cos(angulo),
-     y: Math.sin(angulo)
-   }
+function spawnInimigos() {
+    setInterval(() => {
+        const tamanho = Math.random() * (30 - 5) + 5
+        let x
+        let y
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - tamanho : canvas.width + tamanho
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - tamanho : canvas.height + tamanho
+        }
+        const cor = 'black'
+        const angulo = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velo = {
+            x: Math.cos(angulo),
+            y: Math.sin(angulo)
+        }
 
-    inimigos.push(new Inimigo(x, y, tamanho, cor, velo))
-    } , 1000)
+        inimigos.push(new Inimigo(x, y, tamanho, cor, velo))
+    }, 1000)
 }
 
 function animação() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     jogador.desenhar()
     animaçaoId = requestAnimationFrame(animação)
-    tiros.forEach((tiro) => {
+    tiros.forEach((tiro, index) => {
         tiro.update()
+        //  remover das pontas da tela
+        if (tiro.x + tiro.radius < 0 || 
+            tiro.x - tiro.radius > canvas.width || 
+            tiro.y + tiro.radius < 0 || 
+            tiro.y - tiro.radius > canvas.height) {
+            setTimeout(() => {
+                tiros.splice(index, 1)
+            }, 0)
+        }
+
     })
 
     inimigos.forEach((inimigo, index) => {
         inimigo.update()
-        const dist = Math.hypot(jogador.x - inimigo.x, jogador.y- inimigo.y)
+        const dist = Math.hypot(jogador.x - inimigo.x, jogador.y - inimigo.y)
         // fim de jogo
-          if(dist - inimigo.radius - jogador.radius < 1){
+        if (dist - inimigo.radius - jogador.radius < 1) {
             cancelAnimationFrame(animaçaoId)
-          }
-
-
-        tiros.forEach((tiro, tiroIndex) =>{
-        const distancia = Math.hypot(tiro.x - inimigo.x, tiro.y- inimigo.y)
-        // objetos se tocam
-        if(distancia - inimigo.radius - tiro.radius < 1){
-            setTimeout(() => {
-          inimigos.splice(index, 1)
-          tiros.splice(tiroIndex, 1)
-            }, 0)
         }
+
+
+        tiros.forEach((tiro, tiroIndex) => {
+            const distancia = Math.hypot(tiro.x - inimigo.x, tiro.y - inimigo.y)
+            // objetos se tocam
+            if (distancia - inimigo.radius - tiro.radius < 1) {
+                setTimeout(() => {
+                    inimigos.splice(index, 1)
+                    tiros.splice(tiroIndex, 1)
+                }, 0)
+            }
         })
     })
 }
 // Chamando as funções
 addEventListener('click', (event) => {
     const angulo = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
-    
-   const velo = {
-     x: Math.cos(angulo),
-     y: Math.sin(angulo)
-   }
+
+    const velo = {
+        x: Math.cos(angulo),
+        y: Math.sin(angulo)
+    }
 
     tiros.push(new Projetil(canvas.width / 2, canvas.height / 2, 5, 'silver', velo))
 })
